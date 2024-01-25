5d0272dc95205df9e606004a8996ccf0a2fc97e4
lemonface88 <harrison8818@gmail.com>
2024-01-25 18:05:18 +0000

    commiting test for cleaned data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,3 +14,87 @@ library(tidyverse)
 set.seed(888)
 install.packages("gitcreds")
 library(gitcreds)
+
+# Ref from https://github.com/ThomasWilliamFox/toronto_child_care/blob/master/scripts/00-simulate_data.R
+# Ref from https://github.com/InessaDeAngelis/Toronto_Elections/blob/main/scripts/00-simulate_data.R
+
+# Simulate ticket given data
+# year from 2014-2022
+# ticket counts 1 to 50
+# age group - youth or adult
+
+simulated_ticket_data <-
+  tibble(
+    # randomyl select a year to assign to the ticket_count
+    offence_year = sample(
+      x = c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022),
+      size = 1000,
+      replace = TRUE
+    ), 
+    
+    # randomly assigning each data to either adult or youth
+    age_group =  sample(
+      x = c("Adult", "Youth"),
+      size = 1000,
+      replace = TRUE),
+    
+    # ticket count are randomly generated between 1 to 60 for each data input
+    ticket_count = sample(
+      x = c(1:60),
+      size = 1000,
+      replace = TRUE)
+  )
+
+# cleaned the simulated data for only youth data by per year 
+simulated_youth_data <-
+  simulated_ticket_data|>
+  filter(age_group =="Youth")|>
+  arrange(offence_year) |>
+  summarise(total_violations = sum(ticket_count),
+            .by = offence_year) 
+
+# cleaned the simulated data for only adult data by per year 
+simulated_adult_data <-
+  simulated_ticket_data|>
+  filter(age_group =="Adult")|>
+  arrange(offence_year) |>
+  summarise(total_violations = sum(ticket_count),
+            .by = offence_year) 
+
+# cleaned the simulated data for both adult and youth data by per year 
+simulated_overall_data <-
+  simulated_ticket_data|>
+  arrange(offence_year) |>
+  summarise(total_violations = sum(ticket_count),
+            .by = offence_year) 
+
+
+
+# bar graph of simulated youth data
+simulated_youth_data |> 
+  ggplot(mapping = aes(x = offence_year, y = total_violations))+ 
+  geom_bar(stat= 'identity') +
+  scale_x_continuous(breaks =c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023))+ 
+  labs(title = "Youth Distracted Driving violations Per year", x = "Year", y = "total ticket")
+
+
+# bar graph of simulated adult data
+simulated_adult_data |> 
+  ggplot(mapping = aes(x = offence_year, y = total_violations))+ 
+  geom_bar(stat= 'identity') +
+  scale_x_continuous(breaks =c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023))+ 
+  labs(title = "Adult Distracted Driving violations Per year", x = "Year", y = "total ticket")
+
+# bar graph of overall data
+simulated_overall_data |> 
+  ggplot(mapping = aes(x = offence_year, y = total_violations))+ 
+  geom_bar(stat= 'identity') +
+  scale_x_continuous(breaks =c(2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023))+ 
+  labs(title = "Both Groups Distracted Driving violations Per year", x = "Year", y = "total ticket")
+
+
+
+
+
+
+
---
+++ scripts/03-test_data.R
@@ -0,0 +1,79 @@
+#### Preamble ####
+# Purpose: Tests cleaned data sets 
+# Author: Harrison Huang
+# Date: 25 January 2024
+# Contact: harri.huang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: run 01-download_data.R and 02-data_cleaning.R
+# Any other information needed? If nothing is printed, all tests have passed
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(dplyr)
+library(janitor)
+
+#### Load Cleaned Data Sets ####
+
+# read cleaned youth data
+test_youth_data = read_csv(
+  file = "outputs/data/cleaned_youth_data.csv",
+  show_col_types = FALSE
+)
+
+# read cleaned adult data
+test_adult_data = read_csv(
+  file = "outputs/data/cleaned_adult_data.csv",
+  show_col_types = FALSE
+)
+
+# read cleaned combined youth and adult data
+test_overall_data = read_csv(
+  file = "outputs/data/sum_total_data.csv",
+  show_col_types = FALSE
+)
+
+# Ref from https://github.com/ThomasWilliamFox/toronto_child_care/blob/master/scripts/00-simulate_data.R
+
+### Test the data above ###
+
+## testing youth data ##
+if (min(test_youth_data$ticket_count) < 0) {
+  print("Negative number in ticket count")
+}
+
+if (class(test_youth_data$age_group) != "character") {
+  print("Age group has non-char value")
+}
+
+if (max(test_youth_data$offence_year) < 2024) {
+  print("Incorrect offence year, future")
+}
+
+
+## testing adult data ##
+if (min(test_adult_data$ticket_count) < 0) {
+  print("Negative number in ticket count")
+}
+
+if (class(test_adult_data$age_group) != "character") {
+  print("Age group has non-char value")
+}
+
+if (max(test_adult_data$offence_year) < 2024) {
+  print("Incorrect offence year, future")
+}
+
+## testing overall data ##
+
+if (min(test_overall_data$ticket_count) < 0) {
+  print("Negative number in ticket count")
+}
+
+if (class(test_overall_data$age_group) != "character") {
+  print("Age group has non-char value")
+}
+
+if (max(test_overall_data$offence_year) < 2024) {
+  print("Incorrect offence year, future")
+}
